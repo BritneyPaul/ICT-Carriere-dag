ce9f7fce91a1f0cb4f188d601697f8b8dc89ae12
BritneyPaul <bpaul0122@gmail.com>
2023-12-10 00:09:11 +0100

    fixed topic items content. So each topic has bijbehorende text

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,45 @@ export default function Home() {
   ];
 
   const topicItems = [
-    "Voorlichting Afstudeerprogramma’s",
-    "Voorlichting stage en afstuderen",
-    "Professionele adviezen",
-    "Bedrijvenmarkt",
-    "Gezelligheid"
+    {
+      title: "Voorlichting Afstudeerprogramma’s",
+      dropdownContent: `Het vierde jaar (semester 7 en 8) bestaat uit één semester onderwijs en daarop aansluitend een semester afstuderen.
+    Die twee semesters staan niet los van elkaar. De afstudeeropdracht moet inhoudelijk aansluiten bij het onderwijs van semester 7.
+    
+    Het afstudeerprogramma sluit inhoudelijk en procedureel aan bij het afstuderen en bereid je voor op het afstuderen waarbinnen je zelfstandig aan een complexe opdracht voor een externe organisatie werkt. De keuze voor het vierde jaar maak je al in semester 6.
+    
+    Daarbij kan je kiezen uit 7 afstudeerprogramma's. Dus verdiep je goed en kom naar de voorlichtingssessies van de afstudeerprogramma's die op jouw shortlist staan. Haal bij de welkomstbalie je toegangstickets voor de sessie's die jij bij wilt wonen.`
+    },
+    {
+      title: "Voorlichting stage en afstuderen",
+      dropdownContent: 
+    `Met het zoeken van een stage start je al in het tweede studiejaar. Eind semester 4 moet je stage-opdracht zijn goedgekeurd. Hoe pak je dat aan? Welke stappen moet je daarvoor zetten? Wanneer is een opdrachtgeschikt? Al deze vragen worden beantwoord in de voorlichtingssessies over de stage.
+    
+    Ook voor afstuderen geldt dat er een procedure moet worden gevolgd waarin de opdracht wordt gekeurd en er beoordelaars worden toegewezen. Hoe die procedure eruitziet en hoe je die procedure in gang zet, daarvertellen afstudeercoördinatoren je alles over in de voorlichtingssessies over afstuderen.
+    
+    Haal bij de welkomstbalie je toegangstickets voor de sessies die jij bij wilt wonen.`
+    },
+    {
+      title: "Professionele adviezen",
+      dropdownContent: 
+      `Krijg advies van professionele jobcoaches over hoe je je Linkedin-profiel slim inricht en laat een profielfoto maken door een professionele fotograaf. Zo kan je je nog beter positioneren voor een opdracht of baan die aansluit bij jouw wensen.
+  
+  Ben je bezig met het schrijven van een stageplan of een afstudeerplan? Leg deze voor aan een docent en ga ter plekke aan de slag om het naar een hoger niveau te tillen.
+  
+  Heb jij autisme, dyslexia of een andere superkracht en wil je weten hoe je ie optimaal kunt inzetten om succesvol te studeren? Dan staan de SLB-docenten met hun eigen superkrachten, de zogenaamde Special Educational Needs- begeleiders (SEN) voor je klaar om je te helpen.`
+    },
+    {
+      title: "Bedrijvenmarkt",
+      dropdownContent: 
+      `Zelf contact leggen met een bedrijf voor een stage- of afstudeeropdracht of voor een baan, dat is moeilijk. Op de bedrijvenmarkt staan ze op je te wachten! Geen drempels meer. Jij weet wat je nodig hebt, zij weten waarvoor je komt. Maak kennis met alle 50 deelnemende bedrijven. Ben je eerste- of tweedejaars, geen probleem.
+  
+  Er staan ook bedrijven die bijbaantjes in de aanbieding hebben. Je hoeft niet te wachten tot je derdejaars-stage om praktijkervaring op te doen! Geen toegangsticket nodig voor de bedrijvenmarkt!`
+    },
+    {
+      title: "Gezelligheid",
+      dropdownContent: `We sluiten de dag af met een borrel. Studievereniging SIM zorgt voor de muziek.`
+    }
   ];
-
-  // const menuItemsData = [
-  //   {
-  //     title: 'Home',
-  //     url: '/',
-  //   },
-  //   {
-  //     title: 'Services',
-  //     url: '/services',
-  //   }
-  // ]
 
   const [isOpen, setIsOpen] = useState();
 
@@ -61,7 +83,7 @@ export default function Home() {
           {topicItems.map((item, index) => (
           <li className="topic-item" key={index}>
             <div className="topicItem-container flex items-center justify-between">
-              <span className="hover:text-lightblue">{item}</span>
+              <span className="hover:text-lightblue">{item.title}</span>
             <button
               className="dropdown-button ml-10"
               onClick={() => handleIsOpenClick(index)}
@@ -85,17 +107,8 @@ export default function Home() {
             </button>
             </div>
             <div className={isOpen === index ? 'dropdown-content show' : 'dropdown-content'}>
-              <div className="innertopicText-container text-darkblue text-base font-light">
-                Het vierde jaar (semester 7 en 8) bestaat uit één semester onderwijs en daarop aansluitend een semester afstuderen.
-                Die twee semesters staan niet los van elkaar. De afstudeeropdracht moet inhoudelijk aansluiten bij het onderwijs van semester 7.
-                <br/>
-                <br/>
-                Het afstudeerprogramma sluit inhoudelijk en procedureel aan bij het afstuderen en bereid je voor op het 
-                afstuderen waarbinnen je zelfstandig aan een complexe opdracht voor een externe organisatie werkt. De keuze voor het vierde jaar maak je al in semester 6.
-                <br/>
-                <br/>
-                Daarbij kan je kiezen uit 7 afstudeerprogramma's. Dus verdiep je goed en kom naar de voorlichtingssessies van de afstudeerprogramma's die op jouw shortlist staan. Haal bij 
-                de welkomstbalie je toegangstickets voor de sessie's die jij bij wilt wonen.   
+              <div className="innertopicText-container text-darkblue text-base font-light whitespace-pre-line	">
+                {item.dropdownContent}  
               </div>
             </div>
           </li>
